3cf83f8e7564327ef743603c7a41c8051101447d
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-11-30 12:16:03 -0500

    Add method to shift the alphabet
    A shift factor determines how many letters from the end of the alphabet
    are moved to the start of a temporary array. After those letters are
    added, the remaining letters of the alphabet (from the beginning up to
    the cutoff point) are appended. Because one letter ends up duplicated
    in the process, it is removed to keep the array at 26 letters. The
    method then returns the fully shifted alphabet array.

---
+++ cipher.rb
@@ -0,0 +1,14 @@
+ALPHABET = ('a'..'z').to_a
+
+def shift_alphabet(shift_factor)
+    temp = []
+    ALPHABET.reverse.select.with_index do |letter, index|
+        temp << letter if -index >= -shift_factor 
+    end
+    temp.reverse
+    temp << ('a'..temp[-1]).to_a
+    temp = temp.flatten(1)
+    temp.pop
+    temp
+end
+
