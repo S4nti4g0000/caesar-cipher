fc66da413b92c6fd10062b04216386c51536eb9b
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-12-01 19:13:58 -0500

    Create basic comparison method
    A basic version of the method for comparing the user’s input string with
    the alphabet and returning each letter’s index has been created.
    
    The method converts the input string into an array, stores it in a new
    array, and then uses a nested each.with_index loop to compare each
    character to the letters of the alphabet. When a match is found, it
    prints a message indicating the index of the matching alphabet letter.
    
    Additionally, a bug in the shift-alphabet method was fixed. By returning
    the array with uniq, the method now ensures that the resulting alphabet
    contains no duplicate characters.

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,7 @@
 ALPHABET = ('a'..'z').to_a
+shifted = []
 
-def shift_alphabet(shift_factor)
+def shift_alphabet(shift_factor = 0)
     temp = []
     ALPHABET.reverse.select.with_index do |letter, index|
         temp << letter if -index >= -shift_factor +1
@@ -9,6 +10,18 @@ def shift_alphabet(shift_factor)
     temp << ('a'..temp[-1]).to_a
     temp = temp.flatten(1)
     temp.pop
-    temp
+    temp.uniq
 end
 
+shifted = shift_alphabet(5)
+
+alphabets_container = [ALPHABET, shifted]
+
+def compare_alphabets(str)
+    temp = str.split('')
+    temp.each.with_index do |ch, ind|
+        ALPHABET.each.with_index do |letter, index|
+            puts "Letter #{ch} is in #{index} from Alphabet" if ch == letter
+        end        
+    end    
+end
